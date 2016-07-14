4037f932618e143e8fab4bff733cfcd1e40efa86
Jon Walz <jon.walz.music@gmail.com>
2016-07-14 16:02:16 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// function storefrontStats(location, min, max) {
+//         this.locationName = location;
+//         this.customerHourlyMin = min;
+//         this.customerHourlyMax = max;
+//
+// }
+
+
+var pioneerSquare = {};
+var portlandAirport = {};
+var washingtonSquare = {};
+var sellwood = {};
+var pearlDistrict = {};
