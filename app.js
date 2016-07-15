8a63b385946cfc02e21900f5efba3c1999198fb2
Jon Walz <jon.walz.music@gmail.com>
2016-07-14 18:55:00 -0700

    Javscript logic

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
-// function storefrontStats(location, min, max) {
-//         this.locationName = location;
-//         this.customerHourlyMin = min;
-//         this.customerHourlyMax = max;
-//
-// }
+var pioneerSquare = {
+    // Stores the min/max hourly customers, and the average cookies per customer, in object properties
+    locationName: "Pioneer Square",
+    minHourlyCust: 17,
+    maxHourlyCust: 88,
+    averageSales: 5.2,
+    openHour: 10,
+    closeHour: 18,
 
+    // Uses a method of that object to generate a random number of customers per hour
+    randomCustomersPerHour: function() {
+        var salesPerHour = [];
+        var listBuilt = '<ul>';
+        listBuilt += '<h1>' + this.locationName + '</h1>';
 
-var pioneerSquare = {};
+        for (var i = this.openHour; i <= this.closeHour; i++) {
+
+            // Calculate simulated amounts of cookies purchased for each hour using average cookies purchased and the random number of customers generated
+            var randCookiesSales = Math.random() * (this.maxHourlyCust - this.minHourlyCust);
+            // Store the results for each location in a separate array
+            salesPerHour.push(parseInt(randCookiesSales));
+            // console.log(randCookiesSales);
+            listBuilt += '<li>';
+            // listBuilt += this.openHour;
+            var hour = this.openHour;
+            if (hour < 12) {
+                listBuilt += this.openHour;
+                listBuilt += 'am';
+            } else if (hour == 12) {
+                listBuilt += this.openHour;
+                listBuilt += 'pm';
+            } else {
+                listBuilt += (this.openHour - 12);
+                // hour = (hour - 12);
+                listBuilt += 'pm';
+            }
+            listBuilt += " " + salesPerHour[0] + "<br />";
+            listBuilt += '</li>';
+            this.openHour++;
+
+        }
+        // Display the values of each array as unordered lists in the browser
+        var listsElem = document.getElementById("lists");
+
+        listsElem.innerHTML = listBuilt;
+    }
+};
+
 var portlandAirport = {};
 var washingtonSquare = {};
 var sellwood = {};
