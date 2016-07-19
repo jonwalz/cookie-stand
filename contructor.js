a81bce09a86c1bafc4d285ef12393d9910b42003
Jon Walz <jon.walz.music@gmail.com>
2016-07-18 20:26:17 -0700

    Added locations array

--- contructor.js
+++ contructor.js
@@ -10,3 +10,11 @@ function Location(locationName, minHourlyCust, maxHourlyCust, averageSales, elem
 var pioneerSquare = new Location("Pioneer Square", 17, 88, 5.2);
 
 console.log(pioneerSquare.minHourlyCust);
+
+var locations = [
+    new Location("Pioneer Square", 17, 88, 5.2),
+    new Location("Portland Airport", 6, 24, 1.2),
+    new Location("Washington Square", 11, 38, 1.9),
+    new Location("Sellwood", 20, 48, 3.3),
+    new Location("Pearl District", 3, 24, 2.6)
+];
